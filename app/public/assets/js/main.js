ba3618972fa67aef51d86ee33c844e55d6d3d1a2
Craig Melville <acekreations@gmail.com>
2018-09-10 22:07:20 -0700

    add more complete comments

--- app/public/assets/js/main.js
+++ app/public/assets/js/main.js
@@ -1,17 +1,22 @@
+//onclick listening for form submission
 $("#submit-survey").on("click", function(){
   event.preventDefault();
 
+  //get answers to questions and put them in an array
   var scores = [$("#q1").val().trim(), $("#q2").val().trim(), $("#q3").val().trim(), $("#q4").val().trim(), $("#q5").val().trim(),];
 
+  //create obj to send to the server
   var surveyResults = {
     name: $("#pet-name").val().trim(),
     scores: scores
   };
 
+  //send submission request to server
   $.post("/api/pals", surveyResults)
     .then(function(res){
-      console.log(res);
+      //alert content, displaying match name and image
       var html = '<h3>We found a match!</h3><img src="' + res.img + '"><p>' + res.name + '</p>';
+      //trigger UIKit alert style modal
       UIkit.modal.alert(html);
     });
 
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -6,10 +6,12 @@ var request = require("request");
 //Code to be exported
 module.exports = function(app){
 
+  //return all pals as json
   app.get("/api/pals", function(req, res){
     res.json(pals);
   });
 
+  //handle new submissions
   app.post("/api/pals", function(req, res){
     var newPal = {
       name: req.body.name,
@@ -23,14 +25,19 @@ module.exports = function(app){
       var totalDifference = 0;
       // //loop scores of current(i) pal
       for (var j = 0; j < pals[i].scores.length; j++) {
+        //Find the difference between scores
         var difference = newPal.scores[j] - pals[i].scores[j];
+        //Add to total. Math.abs to ensure the number is positive, this avoids worrying about the order of subtracting scores above
         totalDifference += Math.abs(difference);
       }
+      //Check if there is a new better match
       if (totalDifference < topMatchTotal) {
         topMatchId = totalDifference;
+        //set topMatchId to i. i represents the index of the pal in the pals array
         topMatchId = i;
       }
     }
+    //Query unsplash api for a random dog pic to assign the new pal
     var query = "https://api.unsplash.com/photos/random/?client_id=1cef58c217f78080641b0747a94a8ad92f74893d4f4525c89a25174a4462ee3e&query=dog&count=1";
     request(query, function(err, response){
       if (err) throw err;
@@ -38,9 +45,11 @@ module.exports = function(app){
       var img = response[0].urls.thumb;
       newPal.img = img;
 
+      //Put new submission into array of existing pals
       pals.push(newPal);
     });
 
+    //return best matched pal via id
     return res.json(pals[topMatchId]);
 
   });
